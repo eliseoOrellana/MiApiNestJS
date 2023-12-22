7be9b606cb0fb2beb39d436ec906ccc47f8a4852
root <root@lambo>
2023-12-22 10:27:43 -0300

    cambios en estudiante.controller

--- src/modules/estudiante/estudiante.controller.ts
+++ src/modules/estudiante/estudiante.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { EstudianteEntity } from '../entities/estudiante.entity';
+import { EstudianteService } from './estudiante.service';
 
 @Controller('estudiante')
-export class EstudianteController {}
+export class EstudianteController {
+    constructor(private readonly estudianteService: EstudianteService) {}
+
+    @Get()
+    async findAll(): Promise<EstudianteEntity[]> {
+        return this.estudianteService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<EstudianteEntity> {
+        return this.estudianteService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() estudiante: EstudianteEntity): Promise<EstudianteEntity> {
+        return this.estudianteService.create(estudiante);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() estudiante: EstudianteEntity): Promise<EstudianteEntity> {
+        return this.estudianteService.update(id, estudiante);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: number): Promise<void> {
+        return this.estudianteService.remove(id);
+    }
+}
--- src/modules/estudiante/estudiante.service.ts
+++ src/modules/estudiante/estudiante.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EstudianteEntity } from '../entities/estudiante.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class EstudianteService {}
+export class EstudianteService {
+    constructor(
+        @InjectRepository(EstudianteEntity)
+        private readonly estudianteRepository: Repository<EstudianteEntity>,
+      ) {}
+    
+      async findAll(): Promise<EstudianteEntity[]> {
+        return this.estudianteRepository.find();
+      }
+    
+      async findOne(id: number): Promise<EstudianteEntity> {
+        return this.estudianteRepository.findOneBy({id});
+      }
+    
+      async create(estudiante: EstudianteEntity): Promise<EstudianteEntity> {
+        const newEstudiante = this.estudianteRepository.create(estudiante);
+        return this.estudianteRepository.save(newEstudiante);
+      }
+    
+      async update(id: number, estudiante: EstudianteEntity): Promise<EstudianteEntity>| null {
+        await this.estudianteRepository.update(id, estudiante);
+        return this.estudianteRepository.findOneBy({id});
+      }
+    
+      async remove(id: number): Promise<void> {
+        await this.estudianteRepository.delete(id);
+      }
+    }
