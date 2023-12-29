3d46a74a4697d108122464b47074dc5fbbec4e66
eliseoOrellana <eliseopatricio@gmail.com>
2023-12-29 16:42:01 -0300

    create products

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductosModule } from './modules/productos/productos.module';
+import { ProductsModule } from './modules/products/products.module';
+import { ProductController } from './modules/product/product.controller';
+import { ProductService } from './modules/product/product.service';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
   imports: [
@@ -15,8 +20,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: true,
     }),
+    ProductosModule,
+    ProductsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProductController],
+  providers: [AppService, ProductService],
 })
 export class AppModule {}
