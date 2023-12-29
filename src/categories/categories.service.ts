582200021cd5ab898afefb126869125779ce3e84
MBravoCanales <76415021+MBravoCanales@users.noreply.github.com>
2023-12-29 20:57:25 -0300

    Se crea el categoryDto y crea el metodo createCategory en categories.service

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './category.entity';
+import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+
+    // https://docs.nestjs.com/techniques/database
+    constructor(@InjectRepository(Category) private categoryRepository:Repository<Category> ) {}
+
+    createCategory(category: CreateCategoryDto) {
+        const newCategory = this.categoryRepository.create(category);
+        return this.categoryRepository.save(newCategory);
+    }
+
+}
