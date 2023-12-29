684de731f11423b55f8da613709c9e2e8dc9fa85
MBravoCanales <76415021+MBravoCanales@users.noreply.github.com>
2023-12-29 16:58:45 -0300

    app.module.ts se corrigen conlgictos de importaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-<<<<<<< HEAD
-import { CategoriesModule } from './categories/categories.module';
-=======
-import { ProductosModule } from './modules/productos/productos.module';
 import { ProductsModule } from './modules/products/products.module';
-import { ProductController } from './modules/product/product.controller';
-import { ProductService } from './modules/product/product.service';
-import { ProductsModule } from './modules/products/products.module';
->>>>>>> 3d46a74a4697d108122464b47074dc5fbbec4e66
+import { ProductsController } from './modules/products/products.controller';
+import { ProductsService } from './modules/products/products.service';
 
 @Module({
   imports: [
@@ -24,14 +18,9 @@ import { ProductsModule } from './modules/products/products.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-<<<<<<< HEAD
-    CategoriesModule,
-=======
-    ProductosModule,
     ProductsModule,
->>>>>>> 3d46a74a4697d108122464b47074dc5fbbec4e66
   ],
-  controllers: [AppController, ProductController],
-  providers: [AppService, ProductService],
+  controllers: [AppController, ProductsController],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
