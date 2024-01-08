3a82761aa3acbe63500f72b3c172e2a009a456b2
eliseoOrellana <eliseopatricio@gmail.com>
2024-01-08 00:43:02 -0300

    chore(cors): habilitar CORS para permitir el acceso desde dominios externos

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.enableCors();// Habilitar CORS (Cross-Origin Resource Sharing) para todas las rutas
 }
 bootstrap();
